7a2a284637dac144d381c0e4fb66c240376475e8
TAMILARASAN2308 <140683736+TAMILARASAN2308@users.noreply.github.com>
2024-05-02 15:23:20 +0530

    Delete node js directory

--- node js/http.js
+++
@@ -1,8 +0,0 @@
-let http = require('http');
-http.createServer((request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    response.write(`<h1>hello world...</h1>`);
-    response.end();
-}).listen(3001);
